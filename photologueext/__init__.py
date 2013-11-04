839b0455341bc76f12a3c011346698d3402af8cc
David Bertrand <dbertrand@imagescape.com>
2013-11-04 18:48:17 +0000

    Upgrading for Django 1.5

--- photologueext/__init__.py
+++ photologueext/__init__.py
@@ -1,6 +1,5 @@
 from photologue import models
 
-
 def _index(self):
     for photo in self.public().filter(tags__icontains='index'):
         if 'index' in photo.tags.split():
@@ -8,6 +7,5 @@ def _index(self):
     photos = self.sample(count=1)
     if photos:
         return photos[0]
-
 
 models.Gallery.index_photo = _index
