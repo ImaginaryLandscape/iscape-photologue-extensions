660697d465ada5a2e0793087a0dff088121331b4
David Bertrand <dbertrand@imagescape.com>
2013-11-05 21:25:00 +0000

    Fixed urls patterns

--- photologueext/urls.py
+++ photologueext/urls.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.conf.urls import *
 from django.views.generic import ListView, DetailView
 from photologue.models import Gallery
+from photologue.views import GalleryDetailView
+from photologue.urls import urlpatterns as pl_urlpatterns
 
 # Number of random images from the gallery to display.
 SAMPLE_SIZE = ":%s" % getattr(settings, 'GALLERY_SAMPLE_SIZE', 5)
@@ -11,7 +13,5 @@ urlpatterns = patterns('django.views.generic',
         is_public=True), template_name='photologue/gallery_list.html'),
         name='pl-gallery-index'),
 
-    url(r'^(?P<title_slug>[\-\d\w]+)/$', DetailView.as_view(model=Gallery,
-        template_name='photologue/gallery_detail.html'),
-        name='pl-gallery'),
-)
+#    url(r'^(?P<slug>[\-\d\w]+)/$', GalleryDetailView.as_view(), name='pl-gallery'),
+) + pl_urlpatterns
